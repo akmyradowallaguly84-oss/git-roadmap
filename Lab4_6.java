9ca8a2ae8e1a8d650d6b287448a5049dff8493c8
akmyradowallaguly84-oss <akmyradowallaguly84@gmail.com>
2026-07-03 13:57:37 +0300

    Create Lab4_6.java
    matrix[0][0] diňe saga we aşak düşip bolýar we matrix[n - 1][m - 1] iň gysga ýoly tapmaly

---
+++ Lab4_6.java
@@ -0,0 +1,28 @@
+public class Main{
+    public static void main(String[] args){
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        dp[0][0] = matrix[0][0];
+        for(int i = 1; i < m;i++ ){
+            dp[0][i] = dp[0][i-1]+matrix[0][i];
+        }
+
+        for(int i = 1; i < n;i++){
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+
+        for(int i = 1; i < n;i++){
+            for (int j = 1; j < m;j++){
+                dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        System.out.println(dp[n-1][m-1]);
+    }
+
+}
